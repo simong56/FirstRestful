2aae60e9b80a287405a45e5032db69a09dec30bc
simong56 <simong1@kkszki.hu>
2026-09-17 08:11:11 +0200

    blogger class

---
+++ BlogApi/Models/Blogger.cs
@@ -0,0 +1,11 @@
+namespace BlogApi.Models;
+
+public class Blogger
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public int Age  { get; set; }
+    public string? Password { get; set; }
+    public DateTime RegistrationTime { get; set; }
+}
