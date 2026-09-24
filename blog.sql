d97f21a748cf1dac4d82736b19605a6d2bda1fa0
simong56 <simong1@kkszki.hu>
2026-09-24 10:39:39 +0200

    add create database to db

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 24, 2026 at 10:01 AM
+-- Generation Time: Sep 24, 2026 at 10:39 AM
 -- Server version: 11.4.12-MariaDB
 -- PHP Version: 8.4.25
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `blog`
 --
+CREATE DATABASE IF NOT EXISTS `blog` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `blog`;
 
 -- --------------------------------------------------------
 
